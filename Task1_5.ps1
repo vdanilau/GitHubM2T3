20209d6ab0b1c617c48dc8a189e5072306254156
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-13 00:55:38 +0300

    Create Task1_5.ps1

---
+++ Task1_5.ps1
@@ -0,0 +1,23 @@
+﻿param(
+      [string]$file="C:"
+      )
+
+if (Test-Path $file)
+{
+    Get-Hotfix | where description -EQ "Security Update" |`
+     Export-Csv -Path $file\get-hotfix.csv
+    
+    Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft | `
+    Export-Clixml -Path $file\get-hklm.xml
+}
+
+$s=Import-csv $file\get-hotfix.csv 
+ 
+ForEach ($i in $s)  {Write-Host $i.Description  $i.HotFixID  $i.InstalledOn}
+
+             
+    
+
+
+
+
