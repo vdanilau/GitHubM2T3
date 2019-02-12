00120ca550c82bcecd4578b1b8979cf18618737c
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-12 14:12:01 +0300

    Create Task1_3_1.ps1

---
+++ Task1_3_1.ps1
@@ -0,0 +1,11 @@
+﻿[string]$P=Read-Host ("Path to your Script.ps1")
+$date=get-date
+$Trig= New-JobTrigger -Once -At $date `
+        -RepetitionInterval (New-TimeSpan -Minutes 2) `
+        -RepetitionDuration (New-TimeSpan -Hours 1) 
+
+Register-ScheduledJob -Name Get_CPUTime -FilePath $P `
+                         -Trigger $Trig  
+
+#Remove-JobTrigger -Name "Get_CPU*" 
+#Unregister-ScheduledJob Get_CPUTime  
