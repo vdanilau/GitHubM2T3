8900ebf59eadaff3497fe2a6eb816f20114263dc
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-12 10:23:55 +0300

    Create Task1.ps1

---
+++ Task1.ps1
@@ -0,0 +1,11 @@
+﻿Param(
+       [string]$File="C:\"
+      )
+
+
+if (Test-Path $File )   
+#if (Test-Path "C:\") 
+    {
+     Get-Service | where status -EQ running | `
+     Out-File $file\serv.txt
+     }
