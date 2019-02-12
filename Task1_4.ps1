b5d06e49aa8186e3b43709a9c28b6fdbd96310e4
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-12 15:26:47 +0300

    Create Task1_4.ps1

---
+++ Task1_4.ps1
@@ -0,0 +1,16 @@
+﻿[Cmdletbinding(PositionalBinding=$false)]
+param (
+        [string]$File= "C:\Windows",
+        [string]$EF="*tmp"
+        )
+if (Test-Path $File)
+{
+  $m=Get-ChildItem -Path $File -Recurse -Exclude $EF -Force `
+  -ErrorAction SilentlyContinue | `
+   measure -Property Length -Sum 
+   write-output ("sum=" + $m.sum/1Gb + "Gb")
+}           
+else
+    {
+    Write-Output "The directory doesn't exist"
+    }
