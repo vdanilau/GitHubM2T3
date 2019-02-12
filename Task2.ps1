c4a8b8ef6667276ba2c61dabfaa763cfc3d9a3fa
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-13 00:56:38 +0300

    Create Task2.ps1

---
+++ Task2.ps1
@@ -0,0 +1,16 @@
+﻿New-Item -ItemType file -Path $profile -force
+
+
+ "(Get-Host).UI.RawUI.ForegroundColor = 'green' `n ` #цвет шрифта
+ (Get-Host).UI.RawUI.BackgroundColor = 'black' `n `  #цвет фона
+  Set-Alias HelpMе Get-Help `n `                    #алиас №1
+  Set-Alias gta Get-process `n `                    #алиас №2
+  Set-Variable –name pi –value 3.1415 `n `           #константа 1
+  Set-Variable –name Fid  –value 1,618 `n `          #константа 2
+   Set-Location C:\ `n `                            #Изменить текущую папку
+  Write-Host ″Hello, my friend !!!″ `n `             #Вывести приветсвие
+   " | `
+ Out-File -Append -FilePath $profile                #добавили в профайл
+ notepad $profile                                   #вывели файл профайла
+ Stop-Process -Name powershell_ise                  #остановили PS
+
