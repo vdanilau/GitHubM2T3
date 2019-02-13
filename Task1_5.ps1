ad3231d8655b35178c9d60e571a1bd994d6830cc
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-13 10:00:59 +0300

    Update Task1_5.ps1

--- Task1_5.ps1
+++ Task1_5.ps1
@@ -1,4 +1,10 @@
-﻿param(
+﻿#1.5.	Создать один скрипт, объединив 3 задачи:
+#1.5.1.	Сохранить в CSV-файле информацию обо всех обновлениях безопасности ОС.
+#1.5.2.	Сохранить в XML-файле информацию о записях одной ветви реестра HKLM:\SOFTWARE\Microsoft.
+#1.5.3.	Загрузить данные из полученного в п.1.5.1 или п.1.5.2 
+#файла и вывести в виде списка  разным разными цветами
+
+param(
       [string]$file="C:"
       )
 
@@ -11,14 +17,5 @@ if (Test-Path $file)
     Export-Clixml -Path $file\get-hklm.xml
 }
 
-$s=Import-csv $file\get-hotfix.csv 
- 
-ForEach ($i in $s) { 
-    Write-Host $i.Description  $i.HotFixID  $i.InstalledOn}
-
-             
-    
-
-
-
-
+$s=Import-csv $file\get-hotfix.csv |  fl | Out-String
+Write-Host $s -ForegroundColor Yellow
